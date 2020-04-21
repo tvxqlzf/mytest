ef1c151246a5b628aafeab48113ca3beb089f16e
tvxqlzf <tvxqlzflzf@gmail.com>
2020-04-21 09:34:31 +0900

    前端

---
+++ src/views/edu/teacher/form.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app-container">
+    讲师表单
+  </div>
+
+</template>
+<script>
+export default {
+
+}
+</script>
---
+++ src/views/edu/teacher/list.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app-container">
+    讲师列表
+  </div>
+
+</template>
+<script>
+export default {
+
+}
+</script>
